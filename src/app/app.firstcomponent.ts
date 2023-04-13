f07c6998ba12d0d6aeb77318e80d9832960793db
Somnath Bagul <Somnath.Bagul@BRILLIO.COM>
2023-04-13 16:35:35 +0530

    first demo code

---
+++ src/app/app.firstcomponent.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'firstapp-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class FirstComponent{
+    title = 'firstComponet-demo';
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { FirstComponent } from './app.firstcomponent';
 
 @NgModule({
   declarations: [
-    AppComponent
+    FirstComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    FirstComponent]
 })
 export class AppModule { }
